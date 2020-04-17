fc2a7cb6c2e0f730645804874ff7401e658760e2
dennisclemente <dennisclemente@gmail.com>
2020-04-17 19:33:07 -0400

    added contentful content

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Navbar from "./Globals/Navbar";
-// import Footer from "./Globals/Footer";
+import Footer from "./Globals/Footer";
 import "./layout.css";
 import "./bootstrap.min.css";
 const Layout = ({ children }) => (
   <>
     <Navbar />
     {children}
-    {/* <Footer /> */}
+    <Footer />
   </>
 );
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,26 +4,26 @@ import { Link, graphql } from "gatsby";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import BackgroundSection from "../components/Globals/BackgroundSection";
-// import Info from "../components/Home/Info";
-// import Menu from "../components/Home/Menu";
-// import Products from "../components/Home/Products";
-// import Contact from "../components/Home/Contact";
-// const IndexPage = ({ data }) => {
-//   return (
-//     <Layout>
-//       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-//       <BackgroundSection
-//         img={data.img.childImageSharp.fluid}
-//         title="kape from ph"
-//         styleClass="default-background"
-//       />
-//       <Info />
-//       <Menu items={data.menu} />
-//       <Products />
-//       <Contact />
-//     </Layout>
-//   );
-// };
+import Info from "../components/Home/Info";
+import Menu from "../components/Home/Menu";
+import Products from "../components/Home/Products";
+import Contact from "../components/Home/Contact";
+const IndexPage = ({ data }) => {
+  return (
+    <Layout>
+      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+      <BackgroundSection
+        img={data.img.childImageSharp.fluid}
+        title="kape from ph"
+        styleClass="default-background"
+      />
+      <Info />
+      <Menu items={data.menu} />
+      <Products />
+      <Contact />
+    </Layout>
+  );
+};
 
 export const query = graphql`
   {
